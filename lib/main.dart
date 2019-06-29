20f3281ccb3fd2bc1681decb1c4c2a39fcbbf7b1
Frinkeldoodle <admin@frinkel.tech>
2019-06-29 04:40:16 -0400

    Added refresh capability, update target instance on-the-fly

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'A Fresh Post from TFTown',
+      title: 'The Looking Glass',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -33,9 +33,10 @@ class MyListScreen extends StatefulWidget {
 
 class _MyListScreenState extends State {
   var timeline = new List<Status>();
-
+  String targetInstance = "pleroma.site";
+  final specifyAnInstance = TextEditingController();
   _getUsers() {
-    APIConnector.getTimeline().then((response) {
+    APIConnector.getTimeline("https://" + targetInstance).then((response) {
       setState(() {
         Iterable list = json.decode(response.body);
         timeline = list.map((model) => Status.fromJson(model)).toList();
@@ -49,6 +50,7 @@ class _MyListScreenState extends State {
   }
 
   dispose() {
+    specifyAnInstance.dispose();
     super.dispose();
   }
 
@@ -56,105 +58,150 @@ class _MyListScreenState extends State {
   build(context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Frinkel's Posts and Reblogs"),
-        ),
-        body: ListView.builder(
-          itemCount: timeline.length,
-          itemBuilder: (context, index) {
-            return Card(
-              clipBehavior: Clip.antiAlias,
-              elevation: 5.0,
-              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
-                      child: ListTile(
-                        leading: Image.network(timeline[index].account.avatar),
-                        title: Text(
-                          timeline[index].account.displayName,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(timeline[index].account.acct),
-                      )
+        title: Text("The Looking Glass"),
+    actions: <Widget>[
+    IconButton(
+    icon: Icon(
+    Icons.refresh,
+    semanticLabel: 'Refresh Timeline',
+    ),
+    onPressed: () {
+    _getUsers();
+    print('Refreshed!');
+    },
+    ),
+    ],
+    ),
+    body: Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20,0,8,0),
+                child: TextField(
+                  controller: specifyAnInstance,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: targetInstance,
                   ),
-                  Divider(),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-                    child: Visibility(
-                      visible: timeline[index].subjectText.isNotEmpty,
-                      child: Text(
-                        timeline[index].subjectText,
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(8,0,15,0),
+              child: IconButton(
+                icon: Icon(Icons.forward),
+                onPressed: () {
+                  targetInstance = specifyAnInstance.text;
+                  _getUsers();
+                },
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: timeline.length,
+            itemBuilder: (context, index) {
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5.0,
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+                        child: ListTile(
+                          leading: Image.network(timeline[index].account.avatar),
+                          title: Text(
+                            timeline[index].account.displayName,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(timeline[index].account.acct),
+                        )
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                      child: Visibility(
+                        visible: timeline[index].subjectText.isNotEmpty,
+                        child: Text(
+                          timeline[index].subjectText,
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
                       ),
                     ),
-                  ),
-                  Visibility(
-                    visible: timeline[index].subjectText.isNotEmpty,
-                    child: Divider(),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-                    child: MarkdownBody(data: timeline[index].content),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(24, 8, 24, 20),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(5, 0, 4, 0),
-                          child: Icon(
-                            Icons.reply,
-                            color: Colors.blue,
-                            size: 30.0,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
-                          child: Text(
-                            timeline[index].repliesCount.toString(),
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
-                          child: Icon(
-                            Icons.settings_input_antenna,
-                            color: Colors.green,
-                            size: 30.0,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
-                          child: Text(
-                            timeline[index].reblogsCount.toString(),
-                            style: TextStyle(color: Colors.green),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
-                          child: Icon(
-                            Icons.favorite,
-                            color: Colors.orange,
-                            size: 30.0,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
-                          child: Text(
-                            timeline[index].favouritesCount.toString(),
-                            style: TextStyle(color: Colors.orange),
-                          ),
-                        ),
-                      ],
+                    Visibility(
+                      visible: timeline[index].subjectText.isNotEmpty,
+                      child: Divider(),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ));
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                      child: MarkdownBody(data: timeline[index].content),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(24, 8, 24, 20),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(5, 0, 4, 0),
+                            child: Icon(
+                              Icons.reply,
+                              color: Colors.blue,
+                              size: 30.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
+                            child: Text(
+                              timeline[index].repliesCount.toString(),
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
+                            child: Icon(
+                              Icons.settings_input_antenna,
+                              color: Colors.green,
+                              size: 30.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
+                            child: Text(
+                              timeline[index].reblogsCount.toString(),
+                              style: TextStyle(color: Colors.green),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Colors.orange,
+                              size: 30.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(4, 0, 20, 0),
+                            child: Text(
+                              timeline[index].favouritesCount.toString(),
+                              style: TextStyle(color: Colors.orange),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+    );
   }
 }
--- lib/mstdn_api.dart
+++ lib/mstdn_api.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-const baseUrl = "https://transfurrmation.town";
-
 class APIConnector {
-  static Future getTimeline() {
-//    var url = baseUrl + "/api/v1/timelines/public";
-    var url = baseUrl + "/api/v1/accounts/1/statuses?limit=40";
+  static Future getTimeline(String baseUrl) {
+    var url = baseUrl + "/api/v1/timelines/public?local=true";
+//    var url = baseUrl + "/api/v1/accounts/1/statuses?limit=40";
     return http.get(url);
   }
 }
