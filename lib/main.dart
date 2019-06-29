da4f941d6f484888ee35d4dda1419971577b1c1a
Frinkeldoodle <admin@frinkel.tech>
2019-06-29 05:57:06 -0400

    Liiiinks everywhere! 8D

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 
 import 'dart:convert';
 import 'mstdn_status.dart';
 import 'mstdn_api.dart';
 
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'looking_glass_icons.dart' as AppLogo;
 
@@ -45,6 +48,78 @@ class _MyListScreenState extends State {
     });
   }
 
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            children: <Widget>[
+              Icon(AppLogo.LookingGlass.crystal_ball),
+              Text("  The Looking Glass"),
+            ],
+          ),
+          content: RichText(
+            text: new TextSpan(
+              children: [
+                TextSpan(
+                  text: 'An app for quickly displaying public posts from any '
+                      'Mastodon API-compatible social media network.\n\n'
+                      'App designed and created by ',
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextSpan(
+                  text: 'Frinkeldoodle',
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.of(context).pop();
+                      launch('https://frinkel.tech');
+                    },
+                ),
+                TextSpan(
+                  text: '.\n\n'
+                      'Crystal ball icon made by ',
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextSpan(
+                  text: 'Freepik',
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.of(context).pop();
+                      launch('https://www.freepik.com/');
+                    },
+                ),
+                TextSpan(
+                  text: ' from ',
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextSpan(
+                  text: 'flaticon.com',
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.of(context).pop();
+                      launch('https://www.flaticon.com');
+                    },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ]
+        );
+      },
+    );
+  }
+
   initState() {
     super.initState();
     _getUsers();
@@ -59,7 +134,12 @@ class _MyListScreenState extends State {
   build(context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(AppLogo.LookingGlass.crystal_ball),
+        leading: IconButton(
+          icon: Icon(AppLogo.LookingGlass.crystal_ball),
+          onPressed: () {
+            _showDialog();
+          },
+        ),
         title: Text("The Looking Glass"),
         actions: <Widget>[
           IconButton(
@@ -79,8 +159,15 @@ class _MyListScreenState extends State {
           Row(
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Text('https://'),
+                padding: EdgeInsets.fromLTRB(20, 0, 1, 0),
+                child: Text(
+                    'https://',
+                    style: TextStyle(
+                      color: Colors.green,
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                ),
               ),
               Expanded(
                 child: Padding(
@@ -95,13 +182,17 @@ class _MyListScreenState extends State {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.fromLTRB(8, 0, 15, 0),
-                child: IconButton(
-                  icon: Icon(Icons.forward),
-                  onPressed: () {
-                    targetInstance = specifyAnInstance.text;
-                    _getUsers();
-                  },
+                padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                child: Container(
+                  color: Colors.lightBlueAccent,
+                  child: IconButton(
+                    icon: Icon(Icons.forward),
+                    onPressed: () {
+                      SystemChannels.textInput.invokeMethod('TextInput.hide');
+                      targetInstance = specifyAnInstance.text;
+                      _getUsers();
+                    },
+                  ),
                 ),
               ),
             ],
@@ -126,9 +217,22 @@ class _MyListScreenState extends State {
                           child: ListTile(
                             leading:
                                 Image.network(timeline[index].account.avatar),
-                            title: Text(
-                              timeline[index].account.displayName,
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                            //title: Text(
+                            //  timeline[index].account.displayName,
+                            //  style: TextStyle(fontWeight: FontWeight.bold),
+                            //),
+                            title: RichText(
+                              text: TextSpan(
+                                text: timeline[index].account.displayName,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                  launch(timeline[index].account.url);
+                                  },
+                              ),
                             ),
                             subtitle: Text(timeline[index].account.acct),
                           )),
@@ -199,6 +303,19 @@ class _MyListScreenState extends State {
                               child: Text(
                                 timeline[index].favouritesCount.toString(),
                                 style: TextStyle(color: Colors.orange),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.link,
+                                  color: Colors.black,
+                                  size: 30.0,
+                                ),
+                                onPressed: () {
+                                  launch(timeline[index].url);
+                                },
                               ),
                             ),
                           ],
